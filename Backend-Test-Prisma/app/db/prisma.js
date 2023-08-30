845e910d98054aabaab86d7b9e72eac37cf026e1
Fulano de Tal <joaoofficialpedro@gmail.com>
2023-08-29 21:17:58 -0300

    Restart Backend Prisma

--- Backend-Test-Prisma/app/db/prisma.js
+++
@@ -1,7 +0,0 @@
-// const { PrismaClient } = require('@prisma/client')
-const { PrismaClient } = require('../../prisma/generated/client/edge')
-
-
-const prisma = new PrismaClient()
-
-module.exports = prisma
