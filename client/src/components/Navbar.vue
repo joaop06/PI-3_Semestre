9578f96a8a32fff24bc387c3678d07b297f21447
jgabrielgalvao <josegabriel755@gmail.com>
2023-09-01 20:19:30 -0300

    tentativaSalvarLocalStorage

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,20 +1,20 @@
 <template>
-    <v-app-bar color="red pa-2" :elevation="4" scroll-behavior="elevate">
-      <v-row no-gutters justify="center" align="center">
-          <v-col cols="12">
-          <v-toolbar-title class="text-button text-white" id="titulo">
-            <v-icon @click="toggleDrawer">mdi-menu</v-icon>
-          </v-toolbar-title>
-        </v-col>
-      </v-row>
-    </v-app-bar>
+  <v-app-bar color="red pa-2" :elevation="4" scroll-behavior="elevate">
+    <v-row no-gutters justify="center" align="center">
+      <v-col cols="12">
+        <v-toolbar-title class="text-button text-white" id="titulo">
+          <v-icon @click="toggleDrawer">mdi-menu</v-icon>
+        </v-toolbar-title>
+      </v-col>
+    </v-row>
+  </v-app-bar>
 
-    <v-navigation-drawer v-model="drawerOpen" temporary>
+  <v-navigation-drawer class="mt-2" v-model="drawerOpen" temporary>
       <v-list-item v-for="item in menuItems" :key="item.title" :to="item.route">
         <br>
         <v-list-item-title>{{ item.title }}</v-list-item-title>
       </v-list-item>
-    </v-navigation-drawer>
+  </v-navigation-drawer>
 </template>
 <script>
 export default {
--- client/src/views/NovaPostagem.vue
+++ client/src/views/NovaPostagem.vue
@@ -1,59 +1,69 @@
 <template>
-    <v-layout class="rounded rounded-md">  
-      <v-main>
-        <v-layout class="editor d-flex align-center justify-center" row wrap>
-          <v-flex class="quilleditor">
+  <v-layout class="rounded rounded-md">
+    <v-main>
+      <v-layout class="editor d-flex align-center justify-center" row wrap>
+        <v-form class="quilleditor">
+          <v-flex>
             <QuillEditor class="quilleditor_text" v-model="content" theme="snow" />
           </v-flex>
-        </v-layout>
-      </v-main>
-    </v-layout>
-  </template>
+          <v-flex>
+            <v-btn type="button" onclick="Salvar()">Salvar</v-btn>
+          </v-flex>
+        </v-form>
+      </v-layout>
+    </v-main>
+  </v-layout>
+</template>
   
-  <style>
-  .editor {
-    background-color: rgb(8, 255, 82) !important;
-    height: 39rem;
-    width: 100%;
-  }
+<style>
+.editor {
+  background-color: rgb(8, 255, 82) !important;
+  height: 39rem;
+  width: 100%;
+}
+
+.quilleditor {
+  background-color: white;
+  height: 70%;
+  width: 80%;
+  margin-right: 0;
+  margin-top: -100px !important;
+}
+
+.quilleditor_text {
+  background-color: white;
+  height: 100%;
+  width: 100%;
+}
+</style>
   
-  .quilleditor {
-    background-color: white;
-    height: 70%;
-    width: 80%;
-    margin-right: 0;
-    margin-top: -100px !important;
-  }
-  
-  .quilleditor_text {
-    background-color: white;
-    height: 100%;
-    width: 100%;
-  }
-  </style>
-  
-  <script>
-  export default {
-    components: {
-  
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      drawerOpen: false,
+      content: '',
+      menuItems: [
+        { title: 'Nova postagem', route: '/newpost' },
+        { title: 'Postagens', route: '/postagens' },
+        { title: 'Conta', route: '/dashboard' },
+        { title: 'Home', route: '/inicio' }
+      ],
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawerOpen = !this.drawerOpen;
     },
-    data() {
-      return {
-        drawerOpen: false,
-        content: '',
-        menuItems: [
-          { title: 'Nova postagem', route: '/newpost' },
-          { title: 'Postagens', route: '/postagens' },
-          { title: 'Conta', route: '/dashboard' },
-          { title: 'Home', route: '/inicio' }
-        ],
-      };
-    },
-    methods: {
-      toggleDrawer() {
-        this.drawerOpen = !this.drawerOpen;
-      }
+    Salvar(){
+      localStorage.setItem('conteudo', content);
+      alert(localStorage.getItem('conteudo'));
+      console.log('Conteudo salvo: ', localStorage.getItem('conteudo'));
     }
-  };
-  </script>
+  }
+};
+</script>
   
