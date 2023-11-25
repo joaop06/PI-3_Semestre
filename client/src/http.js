1ae3a7a7bfc34575f7dbae37cfd457333ab1d2a6
joaop06 <joaoofficialpedro@gmail.com>
2023-11-25 16:41:28 -0300

    Fix: URL backend

--- client/src/http.js
+++ client/src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:7000',
-  baseURL: 'https://fjlnsq-7000.csb.app',
+  baseURL: 'https://lnsjcf-7000.csb.app',
 });
 
 export default api;
