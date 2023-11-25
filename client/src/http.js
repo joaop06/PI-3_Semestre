a0f31672cee28bd6268296be5dc43fa3f6a1e26e
joaop06 <joaoofficialpedro@gmail.com>
2023-11-25 16:16:27 -0300

    Feat: Alteração da URL Backend hospedada

--- client/src/http.js
+++ client/src/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000',
+  // baseURL: 'http://localhost:7000',
+  baseURL: 'https://fjlnsq-7000.csb.app',
 });
 
 export default api;
