bb8183921523003583c5b13fe9811cd8b27c3940
joaop06 <joaoofficialpedro@gmail.com>
2023-11-25 16:07:30 -0300

    Feat: Funcionalidades de Alteração de Senha e Deletar Conta

--- client/src/views/admin/Conta.vue
+++ client/src/views/admin/Conta.vue
@@ -1,181 +1,273 @@
 <template>
-    <Navbar />
-    <v-container class="">
-        <v-row>
-            <v-col cols="12" class="coluna">
-                <h1 class="titulo">Seja bem vindo à sua conta!!</h1>
-            </v-col>
-        </v-row>
+  <Navbar />
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="coluna">
+        <h1 class="titulo">Seja bem vindo à sua conta!!</h1>
+      </v-col>
+    </v-row>
 
-        <v-row>
-            <v-col class="d-flex justify-center" cols="12">
-                <v-card class="card">
-                    <div class="mt-10 d-flex justify-center">
-                        <v-avatar class="avatar" size="120">
-                            <v-img :height="100" :src="user.avatar" />
-                        </v-avatar>
-                    </div>
-                    <br>
-                    <h2 class="text d-flex justify-center">{{ user.name }}</h2>
-                    <br>
-                    <p class="text d-flex justify-center">Email: {{ user.email }}</p>
-                    <br>
-                    <div class="d-flex justify-center">
-                        <v-btn class="mx-3 button"  @click="showPasswordDialog = true" title="trocar senha" icon>
-                            <v-icon>mdi-lock</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-3 button" @click="showDeleteDialog = true" title="apagar conta" icon>
-                            <v-icon>mdi-delete</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-3 button" @click="likesview" title="curtidas" icon>
-                            <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-3 button" @click="favsview" title="favoritos" icon>
-                            <v-icon>mdi-star</v-icon>
-                        </v-btn>
-                        <v-btn class="mx-3 button" @click="logout" title="sair da conta" icon>
-                            <v-icon>mdi-logout</v-icon>
-                        </v-btn>
-                    </div>
-                </v-card>
-            </v-col>
-        </v-row>
+    <v-row>
+      <v-col class="d-flex justify-center" cols="12">
+        <v-card class="card">
+          <div class="mt-10 d-flex justify-center">
+            <v-avatar class="avatar" size="120">
+              <v-img :height="100" :src="user.avatar" />
+            </v-avatar>
+          </div>
+          <br />
+          <h2 class="text d-flex justify-center">{{ user.name }}</h2>
+          <br />
+          <p class="text d-flex justify-center">Email: {{ user.email }}</p>
+          <br />
+          <div class="d-flex justify-center">
+            <v-btn
+              class="mx-3 button"
+              @click="showPasswordDialog = true"
+              title="trocar senha"
+              icon
+            >
+              <v-icon>mdi-lock</v-icon>
+            </v-btn>
+            <v-btn
+              class="mx-3 button"
+              @click="showDeleteDialog = true"
+              title="apagar conta"
+              icon
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-btn class="mx-3 button" @click="likesview" title="curtidas" icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn class="mx-3 button" @click="favsview" title="favoritos" icon>
+              <v-icon>mdi-star</v-icon>
+            </v-btn>
+            <v-btn
+              class="mx-3 button"
+              @click="logout"
+              title="sair da conta"
+              icon
+            >
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
 
-        <!-- confirmação para alteração de senha -->
-        <v-dialog v-model="showPasswordDialog" max-width="400">
-            <v-card>
-                <v-card-title>Alterar Senha</v-card-title>
-                <v-card-text>
-                    <v-form @submit="changePassword">
-                        <v-text-field v-model="password" label="Nova Senha" type="password" required></v-text-field>
-                        <v-text-field v-model="confirmPassword" label="Confirmar Nova Senha" type="password"
-                            required></v-text-field>
-                        <v-btn color="primary" type="submit">Salvar Senha</v-btn>
-                    </v-form>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn color="error" @click="showPasswordDialog = false">Cancelar</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+    <!-- confirmação para alteração de senha -->
+    <v-dialog v-model="showPasswordDialog" max-width="400">
+      <v-card>
+        <v-card-title>Alterar Senha</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="newPassword"
+            label="Nova Senha"
+            type="password"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="confirmNewPassword"
+            label="Confirmar Nova Senha"
+            type="password"
+            required
+          ></v-text-field>
+          <v-btn color="primary" @click="changePassword">Salvar Senha</v-btn>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="error" @click="showPasswordDialog = false"
+            >Cancelar</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-        <!-- confirmação para deleção de usuario -->
-        <v-dialog v-model="showDeleteDialog" max-width="400">
-            <v-card>
-                <v-card-title>Tem certeza que deseja apagar?</v-card-title>
-                <v-card-text>
-                    <label>Você perderá todos os seus dados, sem possibilidade de reave-los</label>
-                </v-card-text>
-                <v-card-actions class="buttons">
-                    <v-btn id='buttonDialogE' color="error" @click="showDeleteDialog = false">Cancelar</v-btn>
-                    <v-btn id='buttonDialogD' color="primary" @click="deleteAccount()">Deletar mesmo assim</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </v-container>
+    <!-- confirmação para deleção de usuario -->
+    <v-dialog v-model="showDeleteDialog" max-width="400">
+      <v-card>
+        <v-card-title>Tem certeza que deseja apagar?</v-card-title>
+        <v-card-text>
+          <label
+            >Você perderá todos os seus dados, sem possibilidade de
+            reave-los</label
+          >
+        </v-card-text>
+        <v-card-actions class="buttons">
+          <v-btn
+            id="buttonDialogE"
+            color="error"
+            @click="showDeleteDialog = false"
+            >Cancelar</v-btn
+          >
+          <v-btn id="buttonDialogD" color="primary" @click="deleteAccount()"
+            >Deletar mesmo assim</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- Caixa de Diálogo para Resultados -->
+    <v-dialog v-model="showDialogResult" max-width="30vw">
+      <v-card>
+        <v-card-text
+          class="text-center text-h6 text-pink-darken-4 font-weight-bold"
+        >
+          {{ textDialogResult }}
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
   
 <script>
-import Navbar from '@/components/Navbar.vue';
 import router from "@/router/index";
-import http from '@/http'
+import http from "@/http";
+import Navbar from "../../components/Navbar.vue";
 
 export default {
-    components: {
-        Navbar,
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      usuario: {},
+      user: {
+        name: "",
+        email: "",
+        avatar: "/src/assets/icon.png",
+      },
+      showDeleteDialog: false,
+      showPasswordDialog: false,
+      newPassword: "",
+      confirmNewPassword: "",
+
+      // Caixa de Diálogo
+      showDialogResult: false,
+      textDialogResult: "",
+      typeUser: "",
+    };
+  },
+  methods: {
+    async changePassword() {
+      if (
+        this.newPassword === this.confirmNewPassword &&
+        this.newPassword.length >= 5
+      ) {
+        await http
+          .put(`/user?id=${this.usuario.id}`, {
+            password: this.newPassword,
+          })
+          .then((res) => {
+            // Exibe Caixas de Diálogo com Mensagens de Sucesso
+            this.textDialogResult = "Senha Alterada com Sucesso!";
+            this.showDialogResult = true;
+            this.showPasswordDialog = false;
+            setTimeout(() => {
+              this.showDialogResult = false;
+            }, 1500);
+          })
+          .catch((err) => {
+            console.error(err);
+            this.textDialogResult = "Erro ao alterar senha";
+            this.showDialogResult = true;
+            setTimeout(() => {
+              this.showDialogResult = false;
+            }, 2000);
+          });
+      } else {
+        this.textDialogResult = "Senhas não coincidem";
+        this.showDialogResult = true;
+        setTimeout(() => {
+          this.showDialogResult = false;
+        }, 2000);
+      }
     },
-    data() {
-        return {
-            usuario: '',
-            user: {
-                name: " ",
-                email: " ",
-                avatar: "/src/assets/icon.png",
-            },
-            showDeleteDialog: false,
-            showPasswordDialog: false,
-            password: "",
-            confirmPassword: "",
-        };
+    async deleteAccount() {
+      await http
+        .delete(`/user?id=${this.usuario.id}`)
+        .then((res) => {
+          this.textDialogResult =
+            "Conta deletada com sucesso!\nVocê será redirecionado";
+          this.showDialogResult = true;
+
+          // Remove Dados do Session Storage
+          sessionStorage.removeItem("userData");
+          sessionStorage.setItem("typeUser", "client");
+          sessionStorage.removeItem("userId");
+
+          // Redireciona para a Página Inicial
+          setTimeout(() => {
+            this.showDialogResult = false;
+            router.push({ name: "Home" });
+          }, 2000);
+        })
+        .catch((err) => {
+          this.textDialogResult = "Erro ao Deletar Conta";
+          this.showDialogResult = true;
+          setTimeout(() => {
+            this.showDialogResult = false;
+          }, 2000);
+        });
     },
-    created(){
-        this.usuario = JSON.parse(sessionStorage.getItem('userData'));
-        this.user.name = this.usuario.name;
-        this.user.email = this.usuario.email;
+    editProfile() {
+      // Lógica para editar o perfil aqui
     },
-    methods: {
-        changePassword() {
-            // Lógica para alterar a senha aqui
-            if (this.password === this.confirmPassword) {
-                // Senhas coincidem, faça a alteração aqui
-                this.showPasswordDialog = false;
-            } else {
-                // Senhas não coincidem, trate o erro aqui
-            }
-        },
-        deleteAccount() {
-            // Lógica para deletar a conta aqui
-            
-            // console.log('oi')
-            // console.log('userData ', this.usuario.id);
+    logout() {
+      // Lógica para fazer logout aqui
+      let route = "";
 
-            http.delete(`/user?id=${this.usuario.id}`)
+      sessionStorage.removeItem("userData");
+      sessionStorage.setItem("typeUser", "client");
+      sessionStorage.removeItem("userId");
 
-        },
-        editProfile() {
-            // Lógica para editar o perfil aqui
-        },
-        logout() {
-            // Lógica para fazer logout aqui
-            let route = '';
+      route = "Home";
 
-            sessionStorage.removeItem('userData');
-            sessionStorage.setItem('typeUser', 'client');
-            sessionStorage.removeItem('userId');
-
-            route = 'Home'
-
-            router.push({ name: `${route}` });
-        },
-        likesview(){
-            router.push({name: 'LikeView'});
-        },
-        favsview(){
-            router.push({name: 'FavsView'});
-        },
+      router.push({ name: `${route}` });
     },
+    likesview() {
+      router.push({ name: "LikeView" });
+    },
+    favsview() {
+      router.push({ name: "FavsView" });
+    },
+  },
+  mounted() {
+    this.usuario = JSON.parse(sessionStorage.getItem("userData"));
+    this.user.name = this.usuario.name;
+    this.user.email = this.usuario.email;
+  },
 };
 </script>
   
 <style scoped>
-
-.coluna{
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
+.coluna {
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
 }
 .card {
-    width: 40vw;
-    height: 60vh;
-    border: 2px solid black;
+  width: 40vw;
+  height: 60vh;
+  border: 2px solid black;
 }
 
-.titulo{
-    font-weight: bold;
-    font-family: 'Balsamiq Sans', 'Comic Sans';
+.titulo {
+  font-weight: bold;
+  font-family: "Balsamiq Sans", "Comic Sans";
 }
 
-.button{
-    background-color: #835D3D;
-    color: white;
+.button {
+  background-color: #835d3d;
+  color: white;
 }
 
-.avatar{
-    background-color: white !important;
+.avatar {
+  background-color: white !important;
 }
 
-.text{
-    font-size: 20px;
-    font-family: 'Balsamiq Sans', sans-serif;
+.text {
+  font-size: 20px;
+  font-family: "Balsamiq Sans", sans-serif;
 }
 </style>
